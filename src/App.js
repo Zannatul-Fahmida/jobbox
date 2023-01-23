854901c41c1590f8057dff882a7a48e498eb0d72
zannatul <jannatulfahmida15@gmail.com>
2023-01-23 17:12:06 +0600

    employer registration page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { setUser, toggleLoading } from "./features/auth/authSlice";
+import auth from "./firebase/firebase.config";
 import routes from "./routes/routes";
 
 function App() {
+  const { isLoading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.email));
+      }
+      else{
+        dispatch(toggleLoading())
+      }
+    });
+  }, []);
   return (
     <>
+      <Toaster />
       <RouterProvider router={routes} />
     </>
   );
